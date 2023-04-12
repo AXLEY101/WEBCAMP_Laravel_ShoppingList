2198dabfb2a6b4e330ad91bb282ba704ac25b4d5
FUJIKAWA_TERUAKI <FUJIKAWA@example.com>
2023-04-12 13:59:49 +0000

    管理画面のユーザ一覧機能を書いた

---
+++ resources/views/admin/user/list.blade.php
@@ -0,0 +1,20 @@
+@extends('admin.layout')
+
+{{-- メインコンテンツ --}}
+@section('contets')
+        
+
+        <h1>ユーザ一覧</h1>
+        <table border="1">
+        <tr>
+            <td>ユーザID
+            <td>ユーザ名
+            <td>購入した「買うもの」数
+    @foreach ($users as $user)
+        <tr>
+            <td>{{$user->id}}
+            <td>{{$user->name}}
+            <td>{{$user->task_num}}
+    @endforeach
+        </table>
+@endsection
