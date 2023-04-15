2a9f238f505ffcbee700b22b932e997ec2e6e343
FUJIKAWA_TERUAKI <FUJIKAWA@example.com>
2023-04-15 02:16:51 +0000

    提出課題２回目

--- app/Http/Requests/UserRegisterPostRequest.php
+++ app/Http/Requests/UserRegisterPostRequest.php
@@ -19,7 +19,7 @@ class UserRegisterPostRequest extends FormRequest
             //
             'name' => ['required','max:128'],
             'email' => ['required','email','max:254'],
-            'password' => ['required','max:72'],
+            'password' => ['required','confirmed','max:72'],
         ];
     }
 }
